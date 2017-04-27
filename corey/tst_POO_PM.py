50a2f5ecaa8e8e22e2cc4a7d89430aedfc2d3b9b
Jonatan Vianna <jonatan.vianna@gmail.com>
2017-04-27 18:32:35 -0300

    pm poo

--- corey/tst_POO_PM.py
+++ corey/tst_POO_PM.py
@@ -1,27 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import datetime
 
 class FileLog:
+    def __init__(self, log_path):
+        self.log_path = log_path
 
-    def __init__(self, path, filename):
-        self.path = path
-        self.filename = filename
+    def build_date(self):
+        d = datetime.datetime.now() - datetime.timedelta(hours=24)
+        list_log_to_search = []
 
-    @property
-    def full_logfile_path(self):
-        return "{}{}".format(self.path, self.filename)
+        for i in range(0, 24):
+            if d.day < 10:
+                dia = "0" + str(d.day)
+            else:
+                dia = str(d.day)
 
-    def filtro_id_perm(self, id_perm):
-        pass
+            if d.hour < 10:
+                hora = "0" + str(d.hour)
+            else:
+                hora = str(d.hour)
+
+            if d.month < 10:
+                mes = "0" + str(d.month)
+            else:
+                mes = str(d.month)
+
+            d = d + datetime.timedelta(hours=1)
+
+            list_log_to_search.append(
+                self.log_path + str(d.year) + "-" + mes + "-" + dia + "T" + hora + ":00+00:00")
+
+        return list_log_to_search
 
 
 class MercuryFileLog(FileLog):
-    MERCURY_PATH = "/gfs-log/mail-web-mia/atmail/"
+    MERCURY_PATH = "/gfs-log/mail-web-mia/mercury/mercury.log."
 
-    def __init__(self, path, filename):
-        super().__init__(path, filename)
+    def __init__(self):
+        super().__init__(MercuryFileLog.MERCURY_PATH)
 
+class AtmailFileLog(FileLog):
+    ATMAIL_PATH = "/gfs-log/mail-cmgw-mia/cmgw/mail_cmgw_unified.log."
 
-f = MercuryFileLog(MercuryFileLog.MERCURY_PATH, "joe.txt")
-print(f.full_logfile_path)
+    def __init__(self):
+        super().__init__(AtmailFileLog.ATMAIL_PATH)
+
+m = MercuryFileLog()
+print(m.log_path)
+a = AtmailFileLog()
+
+l = a.build_date()
+
